ee147fd9183af2ba69b06bb9abd2345be7813e57
Your Name <youexample.com>
2026-01-18 20:29:59 -0800

    actualizado

--- api/location.js
+++ api/location.js
@@ -4,44 +4,25 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Endpoint vivo, cabrón. Usa POST para datos reales.');
+  res.send('Endpoint vivo.');
 });
 
 app.post('/', async (req, res) => {
+  console.log('POST iniciado - Debug start');
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   console.log('Datos recibidos:', req.body);
-  console.log('IP:', ip);
+  console.log('IP detectada:', ip);
 
   try {
+    console.log('Llamando API ipapi...');
     const geoRes = await fetch(`https://ipapi.co/${ip}/json/`);
     const geoData = await geoRes.json();
     console.log('Localidad desde IP:', geoData);
   } catch (err) {
-    console.error('Error API:', err);
+    console.error('Error en API ipapi:', err);
   }
 
-  res.sendStatus(200);
-});
-
-module.exports = app;
-
-const express = require('express');
-const fetch = require('node-fetch');
-app.use(express.json());
-
-app.post('/', async (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  console.log('Datos recibidos:', req.body);
-  console.log('IP:', ip);
-
-  try {
-    const geoRes = await fetch(`https://ipapi.co/${ip}/json/`);
-    const geoData = await geoRes.json();
-    console.log('Localidad desde IP:', geoData);
-  } catch (err) {
-    console.error('Error API:', err);
-  }
-
+  console.log('POST terminado - Debug end');
   res.sendStatus(200);
 });
 
